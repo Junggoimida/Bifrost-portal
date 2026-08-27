a327cc4c422ab3de35987b40162403f82b4e8ebf
Junggoimida <tiotnc1312@gmail.com>
2026-08-27 11:35:20 +0700

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'toolhub-v5';
+const CACHE = 'toolhub-v7';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e)=>{
