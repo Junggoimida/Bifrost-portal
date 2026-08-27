ae8124eaca3f8060528d5350bf69af5546033fd1
Junggoimida <tiotnc1312@gmail.com>
2026-08-27 10:31:51 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'toolhub-v5';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (e)=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e)=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+// Network-first: always try to fetch the latest version when online.
+// Only fall back to the cached copy if the network request fails (offline).
+self.addEventListener('fetch', (e)=>{
+  e.respondWith(
+    fetch(e.request)
+      .then(res=>{
+        const resClone = res.clone();
+        caches.open(CACHE).then(c=>c.put(e.request, resClone));
+        return res;
+      })
+      .catch(()=> caches.match(e.request))
+  );
+});
